fbd2eaa93730d980e4762f9b5bf1daaf88627028
basma elokaily <basma.abdelhaleem@x-inity.com>
2025-12-20 18:45:59 +0200

    fixed missing parentheses in as view

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -8,9 +8,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ["id", "username", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
-        def create(self, validated_data):
+    def create(self, validated_data):
             user = User.objects.create_user(**validated_data)
-            return user
+            return user    
         
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
