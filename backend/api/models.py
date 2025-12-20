220885ef415da1ab4c3113bd4e25b31659e2af5f
basma elokaily <basma.abdelhaleem@x-inity.com>
2025-12-20 18:20:47 +0200

    Models and views for the task module, setting up the database using postgrsql

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'pending', 'Pending'
+        IN_PROGRESS = 'in-progress', 'In Progress'
+        COMPLETED = 'completed', 'Completed'
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.PENDING
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="tasks")
+
+    class Meta:
+        ordering = ['-created_at']
+    
+    def __str__(self):
+        return self.title
+    
+    
